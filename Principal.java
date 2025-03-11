1a20d80c70ec9f5d0a945d14a2291127a5d6826a
sam <chugasamuel@gmail.com>
2025-03-11 16:51:52 -0600

    creacion de clase principal en Principal.java - agregar importaciones

---
+++ Principal.java
@@ -0,0 +1,15 @@
+package proyecto_1_analizador_lexico;   
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Principal{
+    public static void main(String[] args){
+        File archivoEntrada = new File("input.txt");
+        File archivoSalida = new File("output.txt");
+    }
+}
