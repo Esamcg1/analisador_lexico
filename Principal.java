9924740c5309fdc0525a1ad7ec7851199dcfebf5
sam <chugasamuel@gmail.com>
2025-03-13 12:36:12 -0600

    logica para archivos en main

--- Principal.java
+++ Principal.java
@@ -11,5 +11,41 @@ public class Principal{
     public static void main(String[] args){
         File archivoEntrada = new File("input.txt");
         File archivoSalida = new File("output.txt");
+        try{
+            BufferedReader lecto = new BufferedReader(new FileReader(archivoEntrada));
+            PrintWriter escritor = new PrintWriter(new FileWriter(archivoSalida));
+
+            Lexer lexer = new lexer(lector);
+            String = resultado = ""; 
+            while (true){
+                Tokens token = lexer.yylex();
+                if (token == null){
+                    resultado += "FIN";
+                    escritor.println(resultado);
+                    System.out.println(resultado);
+                    break;
+                }
+                switch (token){
+                    case ERROR:
+                        resultado += "Error, simbolo no reconocido\n";
+                        break;
+                    case NumeroEntero:
+                    case NumeroDecimal:
+                    case Identificador:
+                    case True:
+                    case False:
+                        resultado += lexer.lexeme + ": Es un " + token + "\n";
+                        break;
+                    default:
+                        resultado += token + "\n";
+                        break;
+                }
+            }
+            escritor.close();
+            lector.close();
+            System.out.println("Analisa Lexico completado, verificar archivo de salida :)");
+        }catch (Exception e){
+            System.out.println("Error al abrir el archivo" + e.getMessage());
+        }
     }
 }
